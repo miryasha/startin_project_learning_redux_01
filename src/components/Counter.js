b57bb19a634cd246d63e23fe15ab4e016c1ef3d6
cat-coder <cat-coder>
2022-04-28 23:58:19 -0500

    buttons made

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,38 @@
+import { useSelector, useDispatch, } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const dispatch = useDispatch();
+  //this will execute by this component and extract form store what we need
+  //with selector react recux make an automatic subscription to the store for us
+  const counter = useSelector(state => state.counter);
+
+ const incrementHandler = () => {
+   dispatch({ type: 'increment'})
+ };
+
+
+ const decrementHandler = () => {
+  dispatch({ type: 'decrement'})
+ };
+
+ const increaseBy5Handler = () => {
+  dispatch({ type: 'increase', amount: 5 })
+ };
+
+
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseBy5Handler}>Increase by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
